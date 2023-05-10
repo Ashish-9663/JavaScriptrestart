f5b8873e192cd909a9d7c2637d9629a7cc0280c3
Ashish-9663 <ashishrajput9663@gmail.com>
2023-05-10 22:43:43 +0530

    6

---
+++ day-5_Apr6/index.js
@@ -0,0 +1,43 @@
+// const x = parseInt(prompt('énter 1st number'));
+// const y = parseInt(prompt('énter 2nd number'));
+// const z = parseInt(prompt('énter 3rd number'));
+
+// document.write('You have entered first numbers :- ',x,'<br/>You have entered second numbers :- ',y,'<br/>You have entered third numbers :- ',z);
+// console.log("x,y,z-->>>",x,y,z);
+// // console.log(x+parseInt(y)+parseInt(z));
+
+// if(x>y && x>z){
+//    console.log("x is greater");
+//    document.write("x is greater");
+// }
+// else if(y>z)
+// {
+//     console.log("y is greater");
+// }
+// else{
+//     console.log("z is greater");
+// }
+
+// const myname = 'ashish';
+// const v = 'rajput' + myname;
+// console.log(v);
+
+// const arr = [3,2,5,8,4,9,1];
+
+// function prtMax(arr){
+//   let m =arr[0];
+//   for(let i=1;i<arr.length;i++){
+//     if(arr[i]>arr[0]){
+//         m=arr[i];
+//     }
+//   }
+//   console.log(m);
+// }
+
+// prtMax(arr);
+
+const get = document.getElementById("h1tag").innerText="not good";
+console.log(get);
+
+const set = document.getElementById("h1tag").innerHTML=" <p id='new'>right now</p>";
+console.log(set);
