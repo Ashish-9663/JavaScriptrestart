c812a8fd5a4157f0616a37029c0d51749b7ff595
Ashish-9663 <ashishrajput9663@gmail.com>
2023-05-22 23:06:43 +0530

    22

--- day-7_Apr8/index.js
+++ day-7_Apr8/index.js
@@ -1,25 +1,155 @@
 
+// // const arr = ['ashish','ashu','gulshan','kumar','rajput'];
+
+// // const classList = ['one','two','three','four','five'];
+ 
+// // for(let i=0;i<arr.length;i++){
+// //     const cld = document.createElement('p');
+// //     cld.innerText=arr[i];
+// //     cld.classList.add(classList[i]);
+// //     const g = document.getElementById('h1tag');
+// //     console.log(cld,g)
+// //     g.appendChild(cld);
+// // }
+
+// const g = document.getElementById('h1tag');
+// // g.innerHTML="<h2 id='h2tag' class= 'two'> hiii</h2>";
+//  g.innerHTML=`<h2 id='h2_1tag' class=three> hiii</h2>`;
+
 // const arr = ['ashish','ashu','gulshan','kumar','rajput'];
 
-// const classList = ['one','two','three','four','five'];
- 
-// for(let i=0;i<arr.length;i++){
-//     const cld = document.createElement('p');
-//     cld.innerText=arr[i];
-//     cld.classList.add(classList[i]);
-//     const g = document.getElementById('h1tag');
-//     console.log(cld,g)
-//     g.appendChild(cld);
+// const photo = ["Chrysanthemum.jpg","Hydrangeas.jpg","Jellyfish.jpg","Koala.jpg","Lighthouse.jpg"]
+
+// // for(let i=0;i<arr.length;i++){
+// //     console.log("hello1",arr[i]);
+// //     console.log("hello2"+arr[i]);
+// //     console.log(`hello3${arr[i]}`);
+// // }
+
+// for(let i =0;i<arr.length;i++)
+// {
+//     const pTag = document.createElement("p");
+//     // pTag.classList.add("photoClass")
+//     pTag.innerHTML=`
+//     <h3>${arr[i]}</h3>
+//     <img class = photoClass src="${photo[i]}"/>`;
+//     // pTag.innerHTML=<h3>arr[i]</h3>,<img src=photo[i]/>;
+//     const getTag = document.getElementById("h2tag");
+//     getTag.appendChild(pTag); 
 // }
 
-const g = document.getElementById('h1tag');
-// g.innerHTML="<h2 id='h2tag' class= 'two'> hiii</h2>";
- g.innerHTML=`<h2 id='h2tag' class=three> hiii</h2>`;
+// const ch = document.getElementById("h2tag");
+
+// function allSet(){
+//     if(ch.style.backgroundColor =="green"){
+//         ch.style.backgroundColor="white";
+//       }
+//       else{
+//           ch.style.backgroundColor="green"; 
+//       }
+// }
+
+// // function allSet(){
+// //     const getSet = document.getElementById("h2tag");
+// //     getSet.style.backgroundColor="green";
+// // }
+
+// const target = document.querySelector("button");
+// target.addEventListener("click",allSet);
+
+
+// const ele = document.getElementById("counter");
+// let count=0;
+// function add(){
+// //  ele.innerText=`${count=count+1}`;
+// count++;
+// ele.innerText=`${count}`;
+
+
+// }
+// function sub(){
+//  ele.innerText=`${count=count-1}`;
+// }
+// const ad = document.getElementById("add");
+// const su = document.getElementById("sub")
+// ad.addEventListener("click",add);
+// su.addEventListener("click",sub);
+
+// ad.addEventListener("mouseover",mouseOver);
+// ad.addEventListener("mouseout",mouseOut);
+// su.addEventListener("mouseout",mouseOver);
+// su.addEventListener("mouseover",mouseOut);
+
+// function mouseOver(){
+//     const ele = document.getElementById("h2tag");
+//     ele.innerText="increaseing counter";
+// }
+
+// function mouseOut(){
+//     const ele = document.getElementById("h2tag");
+//     ele.innerText="decreasing counter";
+// }
+
+// const arr = ['ashish','ashu','gulshan','kumar','rajput'];
+
+//  const photo = ["Chrysanthemum.jpg","Hydrangeas.jpg","Jellyfish.jpg","Koala.jpg","Lighthouse.jpg"]
+//  const classList = ['one','two','three','four','five'];
+ 
+//  function printed(){
+//     for(let i =0;i<arr.length;i++)
+// {
+//     const pTag = document.createElement("p");
+//      pTag.classList.add(classList[i]);
+//     pTag.innerHTML=`
+//     <h3>${arr[i]}</h3>
+//     <img class = photoClass src="${photo[i]}"/>`;
+//     // pTag.innerHTML=<h3>arr[i]</h3>,<img src=photo[i]/>;
+//     const getTag = document.getElementById("h2tag");
+//     getTag.appendChild(pTag); 
+// }
+//  }
+
+//  const btn =document.querySelector("button");
+//  btn.addEventListener("click",printed);
 
 const arr = ['ashish','ashu','gulshan','kumar','rajput'];
+// arr.push("allRight");
+// console.log(arr);
 
-for(let i=0;i<arr.length;i++){
-    console.log("hello1",arr[i]);
-    console.log("hello2"+arr[i]);
-    console.log(`hello3${arr[i]}`);
-}
+// // arr.pop();
+// // console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.unshift("newName");
+// console.log(arr);
+
+// arr.splice(2,3);
+// console.log(arr);
+
+// y=[1,2,3,4]
+// const z=arr.concat(y);
+// console.log(arr.concat(y),z);
+
+// arr.splice(2,0,2,3,4,5);
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
