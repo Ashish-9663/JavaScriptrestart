204f51cb2bdb1ddb5a63c9a9ab35c5d043e6d33c
Ashish-9663 <ashishrajput9663@gmail.com>
2023-06-07 22:30:48 +0530

    new

--- day-12_Apr16/index.js
+++ day-12_Apr16/index.js
@@ -1,155 +1,155 @@
 
-let students = {
-    name :"äshish",//properties =>> format = key:value ---pairs
-    email : "ashishrajput9663@gamil.com",
-    Address : {
-        city :"palwal",
-        state :"haryana"
-    },
-};
+// let students = {
+//     name :"äshish",//properties =>> format = key:value ---pairs
+//     email : "ashishrajput9663@gamil.com",
+//     Address : {
+//         city :"palwal",
+//         state :"haryana"
+//     },
+// };
 
-console.log(students.Address.state);
+// console.log(students.Address.state);
 
-let arr = ["ashish", "ruchi", "kajal"];
+// let arr = ["ashish", "ruchi", "kajal"];
 
-console.log(arr[2]);
+// console.log(arr[2]);
 
 
-const users = [
-    {id:1,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }
-    },
-    {id:2,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }},
-    {id:3,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }},
-    {id:4,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }},
-    {id:5,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }},
-    {id:6,
-    name:"ashish",
-    email:"ruchi@",
-    address:{
-        vill:"agwanpur",
-        city:"palwal",
-        state:"haryana",
-        geo:{
-            lat:2627.92882,
-            lang:627,
-        },
-    }}
-]
+// const users = [
+//     {id:1,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }
+//     },
+//     {id:2,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }},
+//     {id:3,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }},
+//     {id:4,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }},
+//     {id:5,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }},
+//     {id:6,
+//     name:"ashish",
+//     email:"ruchi@",
+//     address:{
+//         vill:"agwanpur",
+//         city:"palwal",
+//         state:"haryana",
+//         geo:{
+//             lat:2627.92882,
+//             lang:627,
+//         },
+//     }}
+// ]
 
-console.log(users[2].address.geo.lat);
+// console.log(users[2].address.geo.lat);
 
-const obj ={
-    address: function(){
-        console.log("object function");
-    },
-    name:{
-        firstname:"ashish",
-        lastname:"rajput"
-    },
-    marks:[100, 98, 67, 56]
-};
+// const obj ={
+//     address: function(){
+//         console.log("object function");
+//     },
+//     name:{
+//         firstname:"ashish",
+//         lastname:"rajput"
+//     },
+//     marks:[100, 98, 67, 56]
+// };
 
-obj.address();
+// obj.address();
 
-console.log(obj.marks[3]);
+// console.log(obj.marks[3]);
 
-const getTag = document.querySelector("h1");
-console.log(getTag);
-getTag.innerText = "Welcome to Geekster";
-getTag.style.backgroundColor="blue";
+// const getTag = document.querySelector("h1");
+// console.log(getTag);
+// getTag.innerText = "Welcome to Geekster";
+// getTag.style.backgroundColor="blue";
 
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-function alterText(){
-    // const newTag = document.createElement("p");
-    // newTag.innerText ="Hello, javascrpt";
+// function alterText(){
+//     // const newTag = document.createElement("p");
+//     // newTag.innerText ="Hello, javascrpt";
 
-    // getTag.appendChild(newTag);
+//     // getTag.appendChild(newTag);
 
-    getTag.innerText ="Hello, javascrpt";
-}
+//     getTag.innerText ="Hello, javascrpt";
+// }
 
-// btn.addEventListener("click",alterText);
+// // btn.addEventListener("click",alterText);
 
-const getPassword = document.getElementById("inputfeild");
+// const getPassword = document.getElementById("inputfeild");
 
-const getShow = document.getElementById("btnShow");
-const getHide = document.getElementById("btnHide");
+// const getShow = document.getElementById("btnShow");
+// const getHide = document.getElementById("btnHide");
 
-getShow.addEventListener("click",show);
-getHide.addEventListener("click",hide);
+// getShow.addEventListener("click",show);
+// getHide.addEventListener("click",hide);
 
-function hide(){
-    getPassword
-}
+// function hide(){
+//     getPassword
+// }
 
-function show(){
-    getPassword
-}
+// function show(){
+//     getPassword
+// }
 
-console.log(getPassword.value);
+// console.log(getPassword.value);
 
 
 const imgtag = document.getElementById("imageTag");
 
 const image = document.createElement("img");
 image.src="https://www.shutterstock.com/image-photo/portrait-family-friends-on-hiking-260nw-1102115744.jpg";
-image.style.width="200px";
+// image.style.width="200px";
 
 const btns1 = document.createElement("button");
 btns1.id="btnFamily";
