68bcebb7ddcc4230c811dd8d733823de88af78f4
Ashish-9663 <ashishrajput9663@gmail.com>
2023-11-18 19:15:23 +0530

    last

---
+++ edrukaRevision/DateTime/index.js
@@ -0,0 +1,21 @@
+
+let tDate = new  Date();
+let nDate = Date.now();
+
+// console.log(tDate, nDate );
+// console.log(tDate.toDateString(), nDate );
+// console.log(tDate.toLocaleString(), nDate );
+// console.log(tDate.toLocaleDateString(), nDate );
+
+// console.log(tDate.toLocaleTimeString(), nDate );
+// console.log(tDate.toTimeString(), nDate );
+
+// console.log(tDate.getTimezoneOffset(), nDate );
+
+const setDateNow = new Date("2023/09/12");
+// const setDateNow = new Date("09/12/2023");
+console.log(setDateNow);
+
+const setDateNow1 = new Date("2023-09-12");
+// const setDateNow1 = new Date("09-12-2023");
+console.log(setDateNow1);
--- edrukaRevision/eventListner/index.js
+++ edrukaRevision/eventListner/index.js
@@ -47,32 +47,97 @@
 //     char.innerText = e.key;
 // })
 
-const vidBtn = document.querySelector("button");
-const divref = document.querySelector("div");
-const vidref = document.querySelector("video");
-vidBtn.textContent = "display video";
+// const vidBtn = document.querySelector("button");
+// const divref = document.querySelector("div");
+// const vidref = document.querySelector("video");
+// vidBtn.textContent = "display video";
 
-vidBtn.addEventListener("click",() =>{
-    divref.classList.remove("hidden");
-})
+// vidBtn.addEventListener("click",() =>{
+//     divref.classList.remove("hidden");
+// })
 
-// vidref.addEventListener("click",() =>{
+// // vidref.addEventListener("click",() =>{
+// //     Video.play();
+// // })
+
+// vidref.addEventListener("click",(e) =>{
+//     e.stopPropagation();
 //     Video.play();
 // })
 
-vidref.addEventListener("click",(e) =>{
-    e.stopPropagation();
-    Video.play();
-})
-
-divref.addEventListener("click",() =>{
-    divref.classList.add("hidden");
-})
+// divref.addEventListener("click",() =>{
+//     divref.classList.add("hidden");
+// })
 
 
-const buttonBar = document.querySelector('.button-bar');
-buttonBar.addEventListener("click",function(e){
-   e.currentTarget.style.backgroundColor = e.currentTarget.textContent;
-})
+// const buttonBar = document.querySelector('.button-bar');
+// buttonBar.addEventListener("click",function(e){
+//    e.currentTarget.style.backgroundColor = e.currentTarget.textContent;
+// })
+
+
+// const bodyTag = document.querySelector("h1");
+// const userInput = document.createElement("input");
+// userInput.type = "text";
+// userInput.id = "inputFild1"
+// const userInput1 = document.createElement("input");
+// userInput1.type = "text";
+// userInput1.id = "inputFild2"
+// const userInput2 = document.createElement("input");
+// userInput2.type = "text";
+// userInput2.id = "inputFild3"
+// const userInput3 = document.createElement("input");
+// userInput3.type = "text";
+// userInput3.id = "inputFild4"
+// const userInput4 = document.createElement("input");
+// userInput4.type = "text";
+// userInput4.id = "inputFild5"
+// const userInput5 = document.createElement("input");
+// userInput5.type = "text";
+// userInput5.id = "inputFild6" 
+// bodyTag.append(userInput, userInput1, userInput2, userInput3, userInput4, userInput5 );
+
+// bodyTag.addEventListener("keydown",(e) => {
+//     console.log(e.target);
+//     console.log(e.type);
+//     console.log(e.key);
+//     console.log(e.ctrlKey);
+//     console.log(e.timeStamp);
+//     console.log(e.currentTarget);
+// })
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+setTimeout(function(){console.log
+("setTimeout1")},0)
+console.log(5);
+console.log(6);
+(function(){console.log("fnction1")}());
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+setTimeout(function(){console.log
+    ("setTimeout2")},6)
+console.log(11);
+console.log(12);
+console.log(13);
+console.log(14);
+setTimeout(function(){console.log
+    ("setTimeout3")},3)
+console.log(15);
+console.log(16);
+console.log(17);
+console.log(18);
+console.log(19);
+console.log(20);
+Promise.resolve().then(() =>{console.log("promises1")})
+for(let i=0;i<20;i++){
+    console.log("loop"+i);
+}
+Promise.resolve().then(() =>{console.log("promises2")})
+
 
 
